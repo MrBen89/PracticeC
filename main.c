1f5e7bf17308eb28e54d98dbe2c4e320d64e57e1
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2022-03-09 10:07:00 +0900

    Add overlay

--- main.c
+++ main.c
@@ -3,18 +3,30 @@
 #include "slime.c"
 #include "background.c"
 #include "backgroundsheet.c"
+#include "windowmap.c"
+#include <gbdk/font.h>
+
 
 void main(){
     UINT8 currentSpriteIndex = 0;
+    font_t min_font;
 
-    set_bkg_data(0, 7, BackgroundTile);
+    font_init();
+    min_font = font_load(font_min); //36 tiles
+    font_set(min_font);
+
+    set_bkg_data(37, 7, BackgroundTile); //load after font data
     set_bkg_tiles(0, 0, 40, 18, BackgroundSheet);
+
+    set_win_tiles(0, 0, 5, 1, WindowMap);
+    move_win(7,120);
 
     set_sprite_data(0, 2, Slime);
     set_sprite_tile(0, 0);
     move_sprite(0, 88, 78);
     SHOW_SPRITES;
     SHOW_BKG;
+    SHOW_WIN;
     DISPLAY_ON;
 
     while(1){
