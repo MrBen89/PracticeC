28f577c1386a82ae73a7c3d69e2d4f31d5eb708c
Ben Pearson <79908678+MrBen89@users.noreply.github.com>
2022-03-09 09:25:58 +0900

    Scrolling Background

--- main.c
+++ main.c
@@ -1,16 +1,24 @@
 #include <gb/gb.h>
 #include <stdio.h>
 #include "slime.c"
+#include "background.c"
+#include "backgroundsheet.c"
 
 void main(){
     UINT8 currentSpriteIndex = 0;
+
+    set_bkg_data(0, 7, BackgroundTile);
+    set_bkg_tiles(0, 0, 40, 18, BackgroundSheet);
 
     set_sprite_data(0, 2, Slime);
     set_sprite_tile(0, 0);
     move_sprite(0, 88, 78);
     SHOW_SPRITES;
+    SHOW_BKG;
+    DISPLAY_ON;
 
     while(1){
+        scroll_bkg(1,0);
         switch(joypad()){
                 case J_LEFT:
                     scroll_sprite(0, -10, 0);
